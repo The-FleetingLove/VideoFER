3d186b7fe1d8d99e23ca6b5c2f62dd2c9b939846
The-FleetingLove <1148322983@qq.com>
2022-02-16 00:27:54 +0800

    变更

--- Aff_Wild2_process.py
+++ Aff_Wild2_process.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 
 def convert_num(str_path):
@@ -39,7 +40,7 @@ csv_eval_img = []
 # 处理训练集
 for _, _, files in os.walk(annotation_train_path):
     sort_files = sorted(files)
-    for file_name in sort_files:
+    for file_name in tqdm(sort_files):
         label_path = os.path.join(annotation_train_path, file_name)  # 标签文件的路径
         img_dir_path = os.path.join(img_path_sum, file_name.split('.')[0]) + '/'  # 对应标签文件的图片目录路径
         data = pd.read_csv(label_path)
@@ -63,27 +64,27 @@ annotation_train = pd.DataFrame({'Path': csv_train_img, 'Valence': csv_train_v, 
 annotation_train.to_csv(os.path.join(data_dir, 'train_annotation.csv'), index=False)  # 结果csv存放路径
 
 # 处理验证集
-for _, _, files in os.walk(annotation_eval_path):
-    sort_files = sorted(files)
-    for file_name in sort_files:
-        label_path = os.path.join(annotation_eval_path, file_name)  # 标签文件的路径
-        img_dir_path = os.path.join(img_path_sum, file_name.split('.')[0]) + '/'  # 对应标签文件的图片目录路径
-        data = pd.read_csv(label_path)
-        valence, arousal = data.iloc[:, 0].values, data.iloc[:, 1].values  # <class 'numpy.ndarray'>
-        va_list = np.array([valence.tolist(), arousal.tolist()]).T  # <class 'numpy.ndarray'>
-        for _, _, img_files_local in os.walk(img_dir_path):
-            img_files = sorted(img_files_local)
-            for sub_file in img_files:
-                if sub_file == '.DS_Store':
-                    continue
-                index = convert_num(sub_file)
-                if va_list[index - 1][0] != -5 and va_list[index - 1][1] != -5:
-                    new_eval_img_path = os.path.join(eval_img_dir, file_name.split('.')[0] + '_' + sub_file)
-                    shutil.copy(os.path.join(img_dir_path, sub_file), new_eval_img_path)
-                    csv_eval_img.append(new_eval_img_path)
-                    csv_eval_v.append(va_list[index - 1][0])
-                    csv_eval_a.append(va_list[index - 1][1])
-                else:
-                    pass
-annotation_eval = pd.DataFrame({'Path': csv_eval_img, 'Valence': csv_eval_v, 'Arousal': csv_eval_a})
-annotation_eval.to_csv(os.path.join(data_dir, 'eval_annotation.csv'), index=False)  # 结果csv存放路径
+# for _, _, files in os.walk(annotation_eval_path):
+#     sort_files = sorted(files)
+#     for file_name in sort_files:
+#         label_path = os.path.join(annotation_eval_path, file_name)  # 标签文件的路径
+#         img_dir_path = os.path.join(img_path_sum, file_name.split('.')[0]) + '/'  # 对应标签文件的图片目录路径
+#         data = pd.read_csv(label_path)
+#         valence, arousal = data.iloc[:, 0].values, data.iloc[:, 1].values  # <class 'numpy.ndarray'>
+#         va_list = np.array([valence.tolist(), arousal.tolist()]).T  # <class 'numpy.ndarray'>
+#         for _, _, img_files_local in os.walk(img_dir_path):
+#             img_files = sorted(img_files_local)
+#             for sub_file in img_files:
+#                 if sub_file == '.DS_Store':
+#                     continue
+#                 index = convert_num(sub_file)
+#                 if va_list[index - 1][0] != -5 and va_list[index - 1][1] != -5:
+#                     new_eval_img_path = os.path.join(eval_img_dir, file_name.split('.')[0] + '_' + sub_file)
+#                     shutil.copy(os.path.join(img_dir_path, sub_file), new_eval_img_path)
+#                     csv_eval_img.append(new_eval_img_path)
+#                     csv_eval_v.append(va_list[index - 1][0])
+#                     csv_eval_a.append(va_list[index - 1][1])
+#                 else:
+#                     pass
+# annotation_eval = pd.DataFrame({'Path': csv_eval_img, 'Valence': csv_eval_v, 'Arousal': csv_eval_a})
+# annotation_eval.to_csv(os.path.join(data_dir, 'eval_annotation.csv'), index=False)  # 结果csv存放路径
